ffb886e45479eb4971058e01d4b541d1e6497961
Roman <rom@kozova.net>
2022-11-28 15:37:31 +0200

    9 task

--- 11/task9.js
+++ 11/task9.js
@@ -1,4 +1,21 @@
 function generateCard(card) {
-  divCard = 
-  document.appendChild('')    
+  const divCard = document.createElement("div");
+  divCard.setAttribute("class", "card");
+  document.appendChild(divCard);
+
+  const h4 = document.createElement("h4");
+  h4.innerText = card.title;
+  divCard.appendChild(h4);
+
+  const br = document.createElement("br");
+  divCard.appendChild(br);
+
+  const image = document.createElement("img");
+  image.setAttribute("src", "Some url");
+  image.setAttribute("alt", "alternative text was not provided");
+  divCard.appendChild(image);
+
+  const p = document.createElement("p");
+  p.innerText = "Eu ut non sint ullamco minim anim laboris sit nulla.";
+  divCard.appendChild(p);
 }
